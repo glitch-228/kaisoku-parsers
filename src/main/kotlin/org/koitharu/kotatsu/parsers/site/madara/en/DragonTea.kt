7f74e3886d7d7fcb3da67676f9f550d5b7eca868
glitch-228 <66838958+glitch-228@users.noreply.github.com>
2026-09-09 22:49:56 +0300

    Point DragonTea at its novel catalog

--- src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/DragonTea.kt
+++ src/main/kotlin/org/koitharu/kotatsu/parsers/site/madara/en/DragonTea.kt
@@ -2,11 +2,23 @@ package org.koitharu.kotatsu.parsers.site.madara.en
 
 import org.koitharu.kotatsu.parsers.MangaLoaderContext
 import org.koitharu.kotatsu.parsers.MangaSourceParser
+import org.koitharu.kotatsu.parsers.config.ConfigKey
 import org.koitharu.kotatsu.parsers.model.MangaParserSource
+import org.koitharu.kotatsu.parsers.network.UserAgents
 import org.koitharu.kotatsu.parsers.site.madara.MadaraParser
 
 @MangaSourceParser("DRAGONTEA", "DragonTea", "en")
 internal class DragonTea(context: MangaLoaderContext) :
 	MadaraParser(context, MangaParserSource.DRAGONTEA, "dragontea.ink") {
 	override val datePattern = "MM/dd/yyyy"
+	override val postReq = false
+	override val listUrl = "novel/"
+	override val tagPrefix = "novel-genre/"
+
+	override val userAgentKey = ConfigKey.UserAgent(UserAgents.KOTATSU)
+
+	override fun onCreateConfig(keys: MutableCollection<ConfigKey<*>>) {
+		super.onCreateConfig(keys)
+		keys.remove(userAgentKey)
+	}
 }
